81f4e6e30f4befb6acd8d89f3c47bd0328b45c63
harokku <crenna.simone@gmail.com>
2019-11-05 15:11:44 +0100

    FIX: Fixed Heroku env config for port and DB
    Introduced test statement to check Heroku's functionality, to deprecate
    after test succeeded

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
-	google.golang.org/api v0.13.0
+	github.com/lib/pq v1.2.0
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
 )
--- server.go
+++ server.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Heroku port from env variable
+	port := os.Getenv("PORT")
+
+	// Heroku Postgres connection and ping
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	checkErrorAndPanic(err)
+
+	defer db.Close()
+
+	err = db.Ping()
+	checkErrorAndPanic(err)
+	fmt.Println("Correctly pinged DB")
+
+	// TODO: Cancel this aftre functionality check on Heroku
+	// Test select to ckeck for DB connection
+	sqlStatement := `SELECT id, name, address FROM suppliers`
+	var id, name, address string
+	row := db.QueryRow(sqlStatement)
+	switch err := row.Scan(&id, &name, &address); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+	case nil:
+		fmt.Println(id, name, address)
+	default:
+		panic(err)
+	}
+
+	//TODO: Line to check if HEROKU db path exist, clean after check
+	fmt.Printf("DBURL: %s", os.Getenv("DATABASE_URL"))
 	e := echo.New()
 
 	e.Static("/docs", "static/docs")
@@ -14,5 +48,11 @@ func main() {
 		return c.String(http.StatusOK, "Pong")
 	})
 
-	e.Logger.Fatal(e.Start(""))
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+func checkErrorAndPanic(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
